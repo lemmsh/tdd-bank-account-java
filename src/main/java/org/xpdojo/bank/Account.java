a43d8f3bec4a1792cd08b6187cb8d1ee2ea9e71a
Pavel Yakunin <pyakunin@gmail.com>
2020-07-16 17:14:30 +0300

    test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,14 +1,36 @@
 package org.xpdojo.bank;
 
-public class Account {
-    private Money balance = new Money(0);
+import java.util.LinkedList;
+import java.util.Optional;
 
-    public Money deposit(int amount) {
-        balance = balance.add(amount);
-        return balance;
+public class Account {
+    private LinkedList<Money> ledger = new LinkedList<>();
+
+    public void deposit(int amount) {
+        if (amount > 0) {
+            ledger.add(new Money(amount));
+        }
     }
+
+    public Optional<Money> withdraw(int amount) {
+        Money e = new Money(-1 * amount);
+        if (balance().amount >= amount) {
+            ledger.add(e);
+            return Optional.of(new Money(amount));
+        } else {
+            return Optional.empty();
+        }
+    }
+
     public Money balance() {
-        return balance;
+        return ledger.stream().reduce(new Money(0), (a, b) -> new Money(a.amount + b.amount));
+    }
+
+    public void transferTo(Account other, int amount) {
+        withdraw(amount).map(m -> {
+            other.deposit(m.amount);
+            return m;
+        });
     }
 
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -16,5 +16,24 @@ public class AccountTest {
 
     }
 
+    @Test
+    public void depositAnAmountToIncreaseTheBalanceAndThenTransfer() {
+
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(10).isEqualTo(account.balance().amount);
+        Account other = new Account();
+        account.transferTo(other, 15);
+        assertThat(10).isEqualTo(account.balance().amount);
+        assertThat(0).isEqualTo(other.balance().amount);
+        account.transferTo(other, 5);
+        assertThat(5).isEqualTo(account.balance().amount);
+        assertThat(5).isEqualTo(other.balance().amount);
+        account.transferTo(other, 5);
+        assertThat(0).isEqualTo(account.balance().amount);
+        assertThat(10).isEqualTo(other.balance().amount);
+
+    }
+
 
 }
